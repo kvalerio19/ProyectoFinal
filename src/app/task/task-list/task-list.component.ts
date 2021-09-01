9cb6a93f38a820c94ec55f136ca4afda50475b6e
kvalerio19 <kvalerio19@hotmail.es>
2021-08-31 21:09:02 -0600

    AddTable

---
+++ src/app/task/task-list/task-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../task.service';
+import { Task } from '../task';
+import { AuthService } from 'src/app/auth/auth.service';
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent implements OnInit {
+
+  public tasks: Array<Task> = [];
+
+
+  constructor(private taskService: TaskService, private authService: AuthService) { }
+
+  
+
+  deleteTask(task: Task){
+    this.taskService.deleteTask(task, this.authService.user.username)
+      .subscribe((next)=>{
+        this.getTasks();
+        alert("tarea eliminada correctamente");
+      }, (error)=>{
+        alert("la tarea no se pudo eliminar");
+      });
+  }
+
+  getTasks(){
+    this.taskService.getTasks(this.authService.user.username)
+    .subscribe((next: Array<Task>)=>{
+      this.tasks = next;
+    }, (error)=>{
+      alert('No se pudieron cargar las tareas');
+    });
+  }
+
+  n
+  ngOnInit(): void {
+    this.getTasks();
+  }
+}
